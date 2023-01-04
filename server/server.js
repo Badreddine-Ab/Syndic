20e7592dc968f7535232b85c6dd98ec1a469e79f
Badreddine-Ab <badreddine@gmail.com>
2023-01-04 16:04:58 +0100

    adding the structure back end

---
+++ server/server.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+
+// const apiError = require('./Utils/apiError')
+// const globalError = require('./Middlewares/errorMiddleware')
+const cors = require('cors');
+const router = require('./Routes/authRouter')
+const routerSyndic= require('./Routes/SyndicRouter')
+const cookieParser = require("cookie-parser");
+
+
+const express = require('express');
+// const apiError = require('./Utils/apiError');
+const app = express()
+
+app.use(cookieParser());
+app.use(cors({ origin:true, credentials:true }));
+app.use(express.json())
+const dbConnection = require("./Config/database")
+app.use('/api/auth',router)
+app.use('/api/syndic',routerManager)
+
+
+
+dbConnection();
+
+app.all('*',(req,res,next) => {
+    next(new apiError(`Can't find this route: ${req.originalUrl}`,400))
+})
+
+// Global error handling middleware
+app.use(globalError);
+
+const port = process.env.PORT || 8081
+const server = app.listen(port, (err)=> {
+    if(!err){
+    console.log(`the port ${port} is running`)
+    }else{
+        console.log(err)
+    }
+})
+
+// Handle errors outside express
+// process.on("unhandledRejection",(err)=> {
+//     console.error(`UnhandledRejection Errors : ${err.name} | ${err.message}`);
+//     server.close(()=> {
+//         console.error('Shutting down....')
+//         process.exit(1)
+//     })
+    
+// })
+
+module.exports = app
